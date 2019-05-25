a83a1b81e03b27f84c8148b90b30e4d128ce2446
Eric McConkie <eric@ericmcconkie.com>
2019-05-24 18:48:54 -0700

    remove config

---
+++ src/config.sample.ts
@@ -0,0 +1,7 @@
+export default {
+    access_token: "xxx",
+    client_id: "xx",
+    client_secret: "xxx",
+    redirect_uri: "http://localhost:3000/authcode"
+}
+
--- src/config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    access_token: "c5069e5add0ad3862effafa36ee0b10370f6ac89",
-    client_id: "21576",
-    client_secret: "c31b7b20d974b0426cb318b1ae5e88e51700a96f",
-    redirect_uri: "http://localhost:3000/authcode"
-}
-
